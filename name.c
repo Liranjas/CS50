2239cbcdb0be497d31fa3e26f8f2fe34b239db3e
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-01-28 14:25:32 +0000

    Sat, Jan 28, 2023, 11:25 AM -03:00

---
+++ name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void)
+{
+    int first, last;
+    printf("type first name: ");
+    scanf("%d",&first);
+    printf("type second name: ");
+    scanf("%d",&last);
+
+    printf("hello, %d %d \n ", first, last);
+}
