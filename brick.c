4edd9bd7776b54c7ec3d9fd26d6104e2455ddb9d
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-02-08 21:27:13 +0000

    Wed, Feb 8, 2023, 6:27 PM -03:00

--- brick.c
+++ brick.c
@@ -2,7 +2,11 @@
 #include <cs50.h>
 int main (void)
 {
-    int x= get_int("brick size: "); //square size
+    int x= get_int("brick size: ");
+    if (x<1)
+    {
+        printf("you dumb motafucka\n");
+    }
     for (int j=0;j<x;j++)
     {
         for (int i=0 ; i<x ; i++)
