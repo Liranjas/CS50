c8273673b101d9c15028f7aacb949ff76f47f09c
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-02-08 19:00:48 +0000

    Wed, Feb 8, 2023, 4:00 PM -03:00

--- brick.c
+++ brick.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 int main (void)
 {
-    int const x=3; //square size
+    int x= get_int("brick size: "); //square size
     for (int j=0;j<x;j++)
     {
         for (int i=0 ; i<x ; i++)
