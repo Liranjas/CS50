bb3dc239f6185b0bff77f096bc73258538d8a849
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-02-08 18:50:34 +0000

    Wed, Feb 8, 2023, 3:50 PM -03:00

--- brick.c
+++ brick.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main (void)
-{t
-    for (int i=0 ; i<3 ; i++)
+{
+    for (int j=0;j<3;j++)
     {
-        printf("#");
+        for (int i=0 ; i<3 ; i++)
+        {
+            printf("#");
+        }
     }
-    printf("\n");
 }
