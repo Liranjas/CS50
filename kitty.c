e0fb4268a1f5fbcf697037a7c2471151e068b979
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-12 22:07:12 +0000

    Wed, Apr 12, 2023, 7:07 PM -03:00

--- kitty.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main (void)
-{
-    printf("keep goin\n");
-}
--- string.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-int main (void)
-{
-    string c = get_string("type something here:");
-    printf("%s\n", c);
-}
