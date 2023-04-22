fae6c33ba7c6010a6dad3eacb867dfb973e845a1
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-22 17:16:28 +0000

    Sat, Apr 22, 2023, 2:16 PM -03:00

--- scores.c
+++ scores.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 int main (void)
 {
-    int scores[3]= get_int("score:");
-    
+    int scores[3];
+    for (i=0; i<3; i++)
+    {
+        scores[i]= get_int("score:");
+    }
+
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2])/(float) 3);
 }
