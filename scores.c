a196cbdd51f9a4917bfabc0781f20b8f9f543cdc
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-24 13:13:50 +0000

    Mon, Apr 24, 2023, 10:13 AM -03:00

--- scores.c
+++ scores.c
@@ -3,11 +3,18 @@
 
 const int N =3;
 
+float average(int array[]);
+
 int main (void)
 {
     int scores [N];
     for (int i=0; i<N; I++)
     {
-        scores [i]= get_int("score:");
+        scores [i]= get_int("score: ");
     }
+    printf("Average: %f\n", average(scores));
+}
+float average(int array[])
+{
+    
 }
