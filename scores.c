e5939fb428ed57ae87df36ad85da5a2fbaf5cec7
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-24 19:30:12 +0000

    Mon, Apr 24, 2023, 4:30 PM -03:00

--- scores.c
+++ scores.c
@@ -4,19 +4,19 @@
 const int N =3;// global variable
 
 float average(int array[]);//implementation hint for compiler
-
+//              --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 int main (void) // as abstraction, we have 1) get_int > to prompt scores (request)
-                //                         2) printf  > to output the 3) average > wich implements the mathematical operations for the average of the array "scores"
+                //                         2) printf  > to output the 3) average (average of array "scores")> function wich implements the mathematical operations for the average of the array "scores"
 {
-    int scores [N]; //that's an array
+    int scores [N]; // scores is an array
     for (int i=0; i<N; i++)
     {
         scores [i]= get_int("score: ");
     }
     printf("Average: %f\n", average(scores));
 }
-
-float average(int array[])
+//     ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+float average(int array[]) // implementation of function "average"
 {
     int sum=0;
     for (int i=0; i<N; i++)
