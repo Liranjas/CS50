5d8ace861e384fcc901a7e177ec6f2ae5d41bcc7
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-24 13:23:46 +0000

    Mon, Apr 24, 2023, 10:23 AM -03:00

--- scores.c
+++ scores.c
@@ -8,7 +8,7 @@ float average(int array[]);//implementation hint for compiler
 int main (void)
 {
     int scores [N];
-    for (int i=0; i<N; I++)
+    for (int i=0; i<N; i++)
     {
         scores [i]= get_int("score: ");
     }
@@ -20,7 +20,7 @@ float average(int array[])
     int sum=0;
     for (int i=0; i<N; i++)
     {
-        sum= sum + array[i]
+        sum= sum + array[i];
     }
     return sum / (float) N;
 }
