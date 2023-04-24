a800b84e6b4c29469bece39eb8b201862941b02f
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-24 12:48:56 +0000

    Mon, Apr 24, 2023, 9:48 AM -03:00

--- scores.c
+++ scores.c
@@ -5,12 +5,5 @@ const int N =3;
 
 int main (void)
 {
-    const int N =3;
-    int scores[3];
-    for (int i=0; i<3; i++)
-    {
-        scores[i]= get_int("score:");
-    }
-
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2])/(float) 3);
+    printf("%d", N);
 }
