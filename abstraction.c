113699c706d32761e1bba58ae22f336ad17eea6a
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-26 21:27:29 +0000

    Wed, Apr 26, 2023, 6:27 PM -03:00

--- abstraction.c
+++ abstraction.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int greet (name)
+int main (void)
+{
+    printf("%s", greet(name));
+}
+
+string greet (name)
 {
     string name= get_string("what's your name? ");
     printf("hello, %s", name);
