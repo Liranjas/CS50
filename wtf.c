8882bd6f7ec913e987329163b1a79c8d0bae1e5e
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-04-29 18:57:44 +0000

    Sat, Apr 29, 2023, 3:57 PM -03:00

--- wtf.c
+++ wtf.c
@@ -1,15 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-string greet (name){
-
+greet (name)
+{
     string name = get_string("what's your name? ");
     printf("hello, %s", name);
-    }
-
+}
 
 int main (void)
 {
-    string greet(name);
+    greet(name);
 }
 
