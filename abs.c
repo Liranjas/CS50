0c99fd73d9c060eb109e09d78e9b161b54675c20
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-05-02 21:14:49 +0000

    Tue, May 2, 2023, 6:14 PM -03:00

--- abs.c
+++ abs.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-string getname(void)
+const char* getname(void);
 
-int i= getname(void);
+int i= getname(name);
 
 int main (void)
 {
     printf("hello, %i", i)
 }
 
-string getname(void)
+string getname(name)
 {
    string name = get_string("what's your name?: ")
    return name;
