0263af321cc149a37baf2676db9de873f4c48bbf
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-05-02 22:00:27 +0000

    Tue, May 2, 2023, 7:00 PM -03:00

--- doubts.c
+++ doubts.c
@@ -1,6 +1,7 @@
 // return is a function? how tf you implement your own functions and libraries
 // how to use a function that you created, i dont have any header file how to define a function ?
 // does the compiler(or C language) knows that i am implementing a function?
+// "parameter list without types only allowed in function definition" HOW TF I DO A FUNCTION DEFINITION
 
 #include <stdio.h>
 
