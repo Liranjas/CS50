c74c779eaaa499bff4c66d482f0bc28693a1de68
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-05-02 21:17:53 +0000

    Tue, May 2, 2023, 6:17 PM -03:00

--- abs.c
+++ abs.c
@@ -2,15 +2,15 @@
 
 const char* getname(void);
 
+string getname(name)
+{
+   const char* name = get_string("what's your name?: ")
+   return name;
+}
+
 int i= getname(name);
 
 int main (void)
 {
     printf("hello, %i", i)
-}
-
-string getname(name)
-{
-   const char* name = get_string("what's your name?: ")
-   return name;
 }
