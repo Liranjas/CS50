431bf20090174809a693209343cf61f6325f5678
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-05-27 17:21:39 +0000

    Sat, May 27, 2023, 2:21 PM -03:00

--- doubts3.c
+++ doubts3.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int main (void)
+{
+
     string n = get_string("what's your name?: \n");
 
-    string printname (string n)
-    {
+    string printname (string n){
         printf("%s",n);
 
     }
-
+}
