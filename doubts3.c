8cfdcbf3358ee318ef3ac00b831154bf0fb2f7ad
Liranjas <97232921+Liranjas@users.noreply.github.com>
2023-05-27 17:22:32 +0000

    Sat, May 27, 2023, 2:22 PM -03:00

--- doubts3.c
+++ doubts3.c
@@ -6,8 +6,8 @@ int main (void)
 
     string n = get_string("what's your name?: \n");
 
-    string printname (string n){
+    string printname (string n)
+    {
         printf("%s",n);
-
     }
 }
